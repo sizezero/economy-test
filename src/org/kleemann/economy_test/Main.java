76176a00c30d95e87d2efb3b4e842db0db74d890
Robert Kleemann <robert@kleemann.org>
2014-05-11 15:59:22 -0700

    added todo for initial terrain

--- src/org/kleemann/economy_test/Main.java
+++ src/org/kleemann/economy_test/Main.java
@@ -4,29 +4,28 @@ import java.awt.Dimension;
 
 import javax.swing.JComponent;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 
 public class Main {
-	
+
 	/**
 	 * Create the GUI and show it. For thread safety, this method should be
 	 * invoked from the event-dispatching thread.
 	 */
 	private static void createAndShowGUI() {
 		// Create and set up the window.
-		JFrame frame = new JFrame("HelloWorldSwing");
+		JFrame frame = new JFrame("Economy Test");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		// Add the ubiquitous "Hello World" label.
-		//JLabel label = new JLabel("Hello World");
+		// JLabel label = new JLabel("Hello World");
 		JComponent comp = new MapComponent();
-		Dimension d = new Dimension(600,400);
+		Dimension d = new Dimension(600, 400);
 		comp.setMinimumSize(d);
 		comp.setPreferredSize(d);
 		frame.getContentPane().add(comp);
 
 		// Display the window.
-		frame.setSize(new Dimension(600,400));
+		frame.setSize(new Dimension(600, 400));
 		frame.pack();
 		frame.setVisible(true);
 	}
@@ -35,6 +34,7 @@ public class Main {
 		// Schedule a job for the event-dispatching thread:
 		// creating and showing this application's GUI.
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			@Override
 			public void run() {
 				createAndShowGUI();
 			}
